9360fb703d5600045a6ea42d82a57f415b5ce393
Marc Chacon <marcchaconb@gmail.com>
2025-07-29 12:11:18 +0200

    bugfix

--- lib/verifactu/cabecera.rb
+++ lib/verifactu/cabecera.rb
@@ -6,9 +6,12 @@ module Verifactu
     def initialize(obligado_emision:, representante: nil, remisionRequerimiento: nil, remisionVoluntaria: nil )
       raise ArgumentError, "obligado_emision is required" if obligado_emision.nil?
       raise ArgumentError, "obligado_emision must be an instance of PersonaFisicaJuridica" unless obligado_emision.is_a?(PersonaFisicaJuridica)
+      raise ArgumentError, "obligado_emision debe tener un NIF" if obligado_emision.nif.nil? || obligado_emision.nif.empty?
       
       
       raise ArgumentError, "representante must be an instance of Representante or nil" unless representante.nil? || representante.is_a?(PersonaFisicaJuridica)
+      raise ArgumentError, "representante debe tener un NIF" if representante && (representante.nif.nil? || representante.nif.empty?)
+      
       raise ArgumentError, "remisionRequerimiento must be an instance of RemisionRequerimiento or nil" unless remisionRequerimiento.nil? || remisionRequerimiento.is_a?(RemisionRequerimiento)
       raise ArgumentError, "remisionVoluntaria must be an instance of RemisionVoluntaria or nil" unless remisionVoluntaria.nil? || remisionVoluntaria.is_a?(RemisionVoluntaria)
 
